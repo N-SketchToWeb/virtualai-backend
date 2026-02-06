f32a2cb47b74100aebbf7e5a0dd293e05441ba14
unknown <nanditanilajagi@gmail.com>
2026-02-06 20:44:26 +0530

    Fix package structure and API endpoint

--- src/main/java/com/nandita/virtualai_backend/VirtualaiBackendApplication.java
+++ src/main/java/com/nandita/virtualai_backend/VirtualaiBackendApplication.java
@@ -1,4 +1,4 @@
-package com.nandita.virtualai;
+package com.nandita.virtualai_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
